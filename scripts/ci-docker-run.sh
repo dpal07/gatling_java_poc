349590b1c58aec3934118535eb9f98ca2ff5cdbb
dpal <dpal@globalization-partners.com>
2022-09-01 10:59:15 +0100

    update Dockerfile

--- scripts/ci-docker-run.sh
+++ scripts/ci-docker-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CLASS=$1
 CONTAINER_NAME="docker-gatling-container"
-IMAGE_NAME="denvazh/docker-gatling"
+IMAGE_NAME="shashikant86/docker-gatling"
 
 
 check_image_exist() {
--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,4 +1,1 @@
-docker run -it --rm -v /home/core/gatling/conf:/opt/gatling/conf \
--v /home/core/gatling/user-files:/opt/gatling/user-files \
--v /home/core/gatling/results:/opt/gatling/results \
-shashikant86/docker-gatling mvn clean gatling:test -Dgatling.simulationClass=UserContextSimulation
+docker run -it --rm -v mvn gatling:test -Dgatling.simulationClass=UserContextSimulation -i denvazh/gatling run $1
